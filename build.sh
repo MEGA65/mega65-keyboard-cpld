c7f90663501529d969cce18a9c490cff54c6da68
Paul Gardner-Stephen <paul@servalproject.org>
2019-06-24 21:06:30 +0200

    add automated build scripts

---
+++ build.sh
@@ -0,0 +1,8 @@
+export DIAMONDDIR=/usr/local/diamond/3.10_x64
+export TEMP=/tmp
+export LSC_INI_PATH=""
+export LSC_DIAMOND=true
+export TCL_LIBRARY=${DIAMONDDIR}/tcltk/lib/tcl8.4
+export FOUNDRY=/usr/local/lscc/diamond/1.3/ispFPGA
+export PATH=$FOUNDRY/bin/nt:$PATH
+${DIAMONDDIR}/bin/lin64/diamondc build.tcl > build.log
