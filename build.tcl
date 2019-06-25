df796cf5945c791ada2afb4aca979f57782bdc72
Paul Gardner-Stephen <paul@servalproject.org>
2019-06-25 20:24:05 +0200

    build JEDEC file

--- build.tcl
+++ build.tcl
@@ -5,4 +5,5 @@ prj_run Map -impl impl1
 prj_run PAR -impl impl1
 prj_run PAR -impl impl1 -task PARTrace
 prj_run Export -impl impl1 -task Bitgen
+prj_run Export -impl impl1 -task Jedecgen
 prj_project close
